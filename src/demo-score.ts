e7fa9c8f22725de715c0eaa8dca22d98de6623a0
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2022-12-13 22:38:17 +0530

    minor demo dep updates

--- src/demo-score.ts
+++ src/demo-score.ts
@@ -1,47 +1,48 @@
-import * as Cord from '@cord.network/sdk'
-import { ScoreType } from '@cord.network/types'
-import { UUID } from '@cord.network/utils'
+import * as Cord from "@cord.network/sdk";
+import { ScoreType } from "@cord.network/types";
+import { UUID } from "@cord.network/utils";
 
 async function main() {
-  await Cord.init({ address: 'ws://127.0.0.1:9944' })
+  await Cord.init({ address: "ws://127.0.0.1:9944" });
+  // await Cord.init({ address: "wss://staging.cord.network" });
 
   // Step 1: Setup Org Identity
-  console.log(`\n❄️  Demo Identities (KeyRing)`)
+  console.log(`\n❄️  Demo Identities (KeyRing)`);
   //3x4DHc1rxVAEqKWSx1DAAA8wZxLB4VhiRbMV997niBckUwSi
-  const sellerIdentity = Cord.Identity.buildFromURI('//Entity', {
-    signingKeyPairType: 'sr25519',
-  })
+  const sellerIdentity = Cord.Identity.buildFromURI("//Entity", {
+    signingKeyPairType: "sr25519",
+  });
   console.log(
     `🏛  Seller Entity (${sellerIdentity.signingKeyType}): ${sellerIdentity.address}`
-  )
-  const deliveryIdentity = Cord.Identity.buildFromURI('//Delivery', {
-    signingKeyPairType: 'sr25519',
-  })
+  );
+  const deliveryIdentity = Cord.Identity.buildFromURI("//Delivery", {
+    signingKeyPairType: "sr25519",
+  });
   console.log(
     `🏛  Delivery Entity (${deliveryIdentity.signingKeyType}): ${deliveryIdentity.address}`
-  )
-  const collectorIdentity = Cord.Identity.buildFromURI('//BuyerApp', {
-    signingKeyPairType: 'ed25519',
-  })
+  );
+  const collectorIdentity = Cord.Identity.buildFromURI("//BuyerApp", {
+    signingKeyPairType: "ed25519",
+  });
   console.log(
     `🧑🏻‍💼 Score Collector (${collectorIdentity.signingKeyType}): ${collectorIdentity.address}`
-  )
-  const requestorIdentity = Cord.Identity.buildFromURI('//SellerApp', {
-    signingKeyPairType: 'ed25519',
-  })
+  );
+  const requestorIdentity = Cord.Identity.buildFromURI("//SellerApp", {
+    signingKeyPairType: "ed25519",
+  });
   console.log(
     `👩‍⚕️ Score Requestor (${requestorIdentity.signingKeyType}): ${requestorIdentity.address}`
-  )
-  const transactionAuthor = Cord.Identity.buildFromURI('//Bob', {
-    signingKeyPairType: 'sr25519',
-  })
+  );
+  const transactionAuthor = Cord.Identity.buildFromURI("//Bob", {
+    signingKeyPairType: "sr25519",
+  });
   console.log(
     `🏢 Transaction Author (${transactionAuthor.signingKeyType}): ${transactionAuthor.address}`
-  )
-  console.log('✅ Identities created!')
+  );
+  console.log("✅ Identities created!");
 
   // Step 2: Create a jounal entry
-  console.log(`\n❄️  Journal Entry `)
+  console.log(`\n❄️  Journal Entry `);
   let journalContent = {
     entity: sellerIdentity.address,
     uid: UUID.generate().toString(),
@@ -50,17 +51,17 @@ async function main() {
     requestor: requestorIdentity.address,
     scoreType: ScoreType.overall,
     score: 3.7,
-  }
-  console.dir(journalContent, { depth: null, colors: true })
+  };
+  console.dir(journalContent, { depth: null, colors: true });
 
   let newJournalEntry = Cord.Score.fromJournalProperties(
     journalContent,
     sellerIdentity
-  )
+  );
 
-  let journalCreationExtrinsic = await Cord.Score.entries(newJournalEntry)
-  console.log(`\n❄️  Transformed Journal Entry `)
-  console.dir(newJournalEntry, { depth: null, colors: true })
+  let journalCreationExtrinsic = await Cord.Score.entries(newJournalEntry);
+  console.log(`\n❄️  Transformed Journal Entry `);
+  console.dir(newJournalEntry, { depth: null, colors: true });
 
   try {
     await Cord.Chain.signAndSubmitTx(
@@ -70,32 +71,35 @@ async function main() {
         resolveOn: Cord.Chain.IS_IN_BLOCK,
         rejectOn: Cord.Chain.IS_ERROR,
       }
-    )
-    console.log('✅ Journal Entry added!')
+    );
+    console.log("✅ Journal Entry added!");
   } catch (e: any) {
-    console.log(e.errorCode, '-', e.message)
+    console.log(e.errorCode, "-", e.message);
   }
 
-  console.log(`\n❄️  Query Chain Scores `)
+  console.log(`\n❄️  Query Chain Scores `);
   const chainScore = await Cord.Score.query(
     journalContent.entity,
     journalContent.scoreType
-  )
-  console.dir(chainScore, { depth: null, colors: true })
-
-  const chainAvgScore = await Cord.Score.queryAverage(
-    journalContent.entity,
-    journalContent.scoreType
-  )
-  console.dir(chainAvgScore, { depth: null, colors: true })
+  );
+  if (chainScore) {
+    console.dir(chainScore, { depth: null, colors: true });
+    const chainAvgScore = await Cord.Score.queryAverage(
+      journalContent.entity,
+      journalContent.scoreType
+    );
+    console.dir(chainAvgScore, { depth: null, colors: true });
+  } else {
+    console.log(`❌`, `Rating Information Not Found - `, journalContent.entity);
+  }
 }
 
 main()
-  .then(() => console.log('\nBye! 👋 👋 👋 '))
-  .finally(Cord.disconnect)
+  .then(() => console.log("\nBye! 👋 👋 👋 "))
+  .finally(Cord.disconnect);
 
-process.on('SIGINT', async () => {
-  console.log('\nBye! 👋 👋 👋 \n')
-  Cord.disconnect()
-  process.exit(0)
-})
+process.on("SIGINT", async () => {
+  console.log("\nBye! 👋 👋 👋 \n");
+  Cord.disconnect();
+  process.exit(0);
+});
